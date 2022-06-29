b5111a217ccaea059e6a3fb5dc3ebef235538662
ridwan <irfanridwan20@gmail.com>
2022-06-29 22:11:23 +0600

    added handleSubmit function to handle form submit and fixed a bug in label element

--- src/components/form.js
+++ src/components/form.js
@@ -5,17 +5,27 @@ class Form extends Component{
         super(props)
     }
 
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        const firstName=e.target.childNodes[1].value;
+        const lastName=e.target.childNodes[3].value;
+        const email=e.target.childNodes[5].value;
+        const phone=e.target.childNodes[7].value;
+        this.props.input({firstName,lastName,email,phone})
+    }
+
     render(){
         return(
-            <form>
-                <label for='firstName'>First Name</label>
+            <form onSubmit={this.handleSubmit}>   
+                <label htmlFor='firstName'>First Name</label>
                 <input type='text' name='firstName' id='firstName' required></input>
-                <label for='lastName'>Last Name</label>
+                <label htmlFor='lastName'>Last Name</label>
                 <input type='text' name='lastName' id='lastName' required></input>
-                <label for='email'>Email</label>
+                <label htmlFor='email'>Email</label>
                 <input type='email' name='email' id='email' required></input>
-                <label for='phone'>Phone Number</label>
+                <label htmlFor='phone'>Phone Number</label>
                 <input type='tel' name='phone' id='phone' required></input>
+                <button type='submit'>Save</button>
             </form>
         )
     }
