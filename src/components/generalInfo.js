ee5ff3c42ca27e5d57656bf5cf8a7c81ce720f14
ridwan <irfanridwan20@gmail.com>
2022-06-29 22:23:28 +0600

    added code to show the information inputted on the form after the save button is clicked

--- src/components/generalInfo.js
+++ src/components/generalInfo.js
@@ -1,19 +1,38 @@
 import {Component} from 'react';
 import Form from './form';
-import SaveBtn from './saveBtn';
-import EditBtn from './editBtn';
 
 class GeneralInfo extends Component{
   constructor(props){
     super(props);
+    this.state={
+      info:'',
+    }
   }
+
+  getInput=(input)=>{
+    this.setState({
+      info:input,
+    })
+  }
+
   render(){
     return(
         <div>
             <h2>General Information</h2>
-            <Form/>
-            <SaveBtn/>
-            <EditBtn/>
+            {this.state.info==='' &&
+                <div>
+                  <Form input={this.getInput}/>
+                </div> 
+            }
+
+            {this.state.info!=='' &&
+              <div>
+                <p>First Name:{this.state.info.firstName}</p>
+                <p>Last Name:{this.state.info.lastName}</p>
+                <p>Email:{this.state.info.email}</p>
+                <p>Phone:{this.state.info.phone}</p>
+              </div>
+            }
         </div>
     )
   }
