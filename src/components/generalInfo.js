65a97aee892b4d75cf1cf199c263636a32e9d87c
ridwan <irfanridwan20@gmail.com>
2022-06-29 22:34:16 +0600

    added edit button and added code to make edit button work

--- src/components/generalInfo.js
+++ src/components/generalInfo.js
@@ -1,17 +1,26 @@
 import {Component} from 'react';
 import Form from './form';
+import EditBtn from './editBtn';
 
 class GeneralInfo extends Component{
   constructor(props){
     super(props);
     this.state={
       info:'',
+      editOn:false
     }
   }
 
   getInput=(input)=>{
     this.setState({
       info:input,
+      editOn:false
+    })
+  }
+
+  handleEdit=()=>{
+    this.setState({
+      editOn:true
     })
   }
 
@@ -19,18 +28,23 @@ class GeneralInfo extends Component{
     return(
         <div>
             <h2>General Information</h2>
-            {this.state.info==='' &&
+            {this.state.info==='' && this.state.editOn===false &&
                 <div>
-                  <Form input={this.getInput}/>
+                  <Form input={this.getInput} previousValues={this.state.info}/>
                 </div> 
             }
-
-            {this.state.info!=='' &&
+            {this.state.info!=='' && this.state.editOn===true &&
+                <div>
+                  <Form input={this.getInput} previousValues={this.state.info}/>
+                </div>
+            }
+            {this.state.info!=='' && this.state.editOn===false &&
               <div>
                 <p>First Name:{this.state.info.firstName}</p>
                 <p>Last Name:{this.state.info.lastName}</p>
                 <p>Email:{this.state.info.email}</p>
                 <p>Phone:{this.state.info.phone}</p>
+                <EditBtn onClick={this.handleEdit}/>
               </div>
             }
         </div>
