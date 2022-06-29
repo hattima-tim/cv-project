cb845b2f4f219b6d7035cdb7a1625d100ffec7de
ridwan <irfanridwan20@gmail.com>
2022-06-29 10:49:45 +0600

    added GeneralInfo component and a header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import {Component} from 'react';
+import GeneralInfo from './components/generalInfo'
 
 class App extends Component{
   constructor(props){
     super(props);
   }
   render(){
-    
+    return(
+      <div>
+        <h1>Cv Project</h1>
+        <GeneralInfo />
+      </div>
+    )
   }
 }
 
