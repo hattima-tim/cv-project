ace17518aac78e1f43e1222414032c948f0f1080
ridwan <irfanridwan20@gmail.com>
2022-07-01 23:32:47 +0600

    added experienceFactory component to display multiple experience section

---
+++ src/components/eduExperience/experienceFactory.js
@@ -0,0 +1,33 @@
+import {Component} from 'react';
+import Form from './form';
+import uniqid from 'uniqid';
+
+class ExperienceFactory extends Component{
+
+    render(){
+        const {expContainer,handleEdit,placesWhereEditIsOpen} = this.props;
+        const expList = expContainer.map((exp,index)=>{
+            const id=uniqid();
+            const editIsOnForThisExp=placesWhereEditIsOpen.find((editExp)=> JSON.stringify(editExp[0])===JSON.stringify(exp));
+            //editExp=[the exp being edited,it's index in expContainer]
+            //editIsOnForThisExp has two possible value
+            //undefined: when the current exp from map loop is not being edited
+            //exp: when the current exp from map loop is being edited
+            if(editIsOnForThisExp){
+                return <Form key={id} editedExpNum={index} input={this.props.input} previousValues={editIsOnForThisExp[0]}/>
+            }
+            return(
+                <div key={id}>
+                <p>School Name:{exp.schoolName}</p>
+                <p>Title of study:{exp.titleOfStudy}</p>
+                <p>From:{exp.from}</p>
+                <p>To:{exp.to}</p>
+                <button id={index} onClick={handleEdit}>Edit</button>
+                </div>
+            )
+        });
+        return <div>{expList}</div>
+    }
+}
+
+export default ExperienceFactory;
