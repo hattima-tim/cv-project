2e5465ac1df00d32f45ec4760c7dac1a894732dc
ridwan <irfanridwan20@gmail.com>
2022-07-01 23:30:05 +0600

    added and removed code to make edit button work

--- src/components/eduExperience/eduExperience.js
+++ src/components/eduExperience/eduExperience.js
@@ -1,48 +1,59 @@
 import {Component} from 'react';
 import Form from './form';
-import EditBtn from '../editBtn';
 import './eduExperience.css'
+import ExperienceFactory from './experienceFactory';
 
 class eduExp extends Component{
   constructor(props){
     super(props);
     this.state={
-      info:'',
-      editOn:false
+      expContainer:[],
+      placesWhereEditIsOpen:[], //this will hold all the exp that are being edited
     }
   }
 
-  getInput=(input)=>{
-    this.setState({
-      info:input,
-      editOn:false
-    })
+  handleInput=(input,editedExpNumberForEditedSubmit)=>{
+    if(editedExpNumberForEditedSubmit!==undefined){
+      this.setState((prevState)=>({
+        expContainer:prevState.expContainer.map((exp,index)=>{
+          if(index===editedExpNumberForEditedSubmit){
+            //editedExpNumber is calculated based on it's index in expContainer
+            return input; //means this input is updated/edited,so replace it with old one
+          }else{
+            return exp; 
+          }
+        }),
+        placesWhereEditIsOpen:prevState.placesWhereEditIsOpen.filter((expWhereEditisOpen)=>{
+          return expWhereEditisOpen[1]!==editedExpNumberForEditedSubmit;
+        }),
+      }))
+    }else{
+      this.setState((prevState)=>({
+        expContainer:[...prevState.expContainer,input],
+      }))
+    }
   }
 
-  handleEdit=()=>{
-    this.setState({
-      editOn:true
-    })
+  handleEdit=(event)=>{
+    const theExpWhereEditIsHappening=this.state.expContainer[event.target.id];
+    const expNumber=Number(event.target.id);
+    this.setState((prevState)=>({
+      placesWhereEditIsOpen:[...prevState.placesWhereEditIsOpen,[theExpWhereEditIsHappening,expNumber]]
+    }))
   }
 
   render(){
     return(
         <div className='eduExp_container'>
             <h2>Educational Experience</h2>
-            {this.state.info==='' && this.state.editOn===false &&
-                  <Form input={this.getInput} previousValues={this.state.info}/>
+            {this.state.expContainer.toString()==='' && this.state.placesWhereEditIsOpen.toString()==='' &&
+                  //[].toString()=''(an empty string)
+                  <Form input={this.handleInput} previousValues={this.state.expContainer}/>
+                  //the previousValues prop in upper line is only there to prevent error in
+                  //the Form component that previousValues is undefined.
             }
-            {this.state.info!=='' && this.state.editOn===true &&
-                  <Form input={this.getInput} previousValues={this.state.info}/>
-            }
-            {this.state.info!=='' && this.state.editOn===false &&
-              <div>
-                <p>School Name:{this.state.info.schoolName}</p>
-                <p>Title of study:{this.state.info.titleOfStudy}</p>
-                <p>From:{this.state.info.from}</p>
-                <p>To:{this.state.info.to}</p>
-                <EditBtn onClick={this.handleEdit}/>
-              </div>
+            {this.state.expContainer.toString &&
+              <ExperienceFactory input={this.handleInput} placesWhereEditIsOpen={this.state.placesWhereEditIsOpen} expContainer={this.state.expContainer} handleEdit={this.handleEdit}/>
             }
         </div>
     )
