6f5ac1b2319c97ddbaf3b2445826df15921c668a
hattima-tim <irfanridwan20@gmail.com>
2022-07-04 12:21:28 +0000

    added the html structure that will be printed

--- src/components/print/componentToPrint.js
+++ src/components/print/componentToPrint.js
@@ -1,11 +1,49 @@
 import { Component } from'react'
 import './printStyle.css'
+import uniqid from 'uniqid';
 
 class ComponentToPrint extends Component {
-    render() {
+  render() {
+    const { generalInfo, eduExperiences, workExperiences } = this.props;
       return (
         <div>
-
+            <div className='header'>
+              <h1>{`${generalInfo.firstName} ${generalInfo.lastName}`}</h1>
+              <div className='contact'>
+                <p>Email: {generalInfo.email}</p>
+                <p>Phone: {generalInfo.phone}</p>
+              </div>
+            </div>
+          
+            <div className='education_container'>
+              <h1>Education</h1>
+              {
+                eduExperiences.map((exp) => {
+                  const uniqKey=uniqid()
+                  return (
+                    <div key={uniqKey} className='eduInfo'>
+                      <p>{exp.titleOfStudy}</p>
+                      <p>{ `${exp.schoolName} | ${exp.from}-${exp.to}` }</p>
+                    </div>
+                    )
+                  })
+              }
+            </div>
+            
+            <div className='work_container'>
+              <h1>Work Experience</h1>
+              {
+                workExperiences.map((exp) => {
+                  const uniqKey=uniqid()
+                  return (
+                    <div key={uniqKey} className='workExperiences'>
+                      <p>{exp.positionTitle}</p>
+                      <p>{ `${exp.companyName} | ${exp.from}-${exp.to}` }</p>
+                    </div>
+                    )
+                  })
+              }
+            </div>  
         </div>
       );
     }
