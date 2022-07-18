789fcf2eb2c2bbb8329a0e5822a71593357a01e3
hattima-tim <irfanridwan20@gmail.com>
2022-07-18 13:30:01 +0000

    rewrite file
    rewrote the file to use function component and to become the file which
    contains logic for showing saved (by user) information and information
    editing form of both educational experience and work experience

--- src/components/workExperience/experienceFactory.js
+++ src/components/workExperience/experienceFactory.js
@@ -1,39 +1,80 @@
-import {Component} from 'react';
-import Form from './form';
-import uniqid from 'uniqid';
+/* eslint-disable react/prop-types */
+import uniqid from "uniqid";
+import Form from "./form";
 
-class ExperienceFactory extends Component{
+function ExperienceFactory({
+  expContainer,
+  handleEdit,
+  placesWhereEditIsOpen,
+  input,
+  handleDelete,
+  componentIsCalledFrom,
+}) {
+  const getTodayDate = () => {
+    let todayDate = new Date();
+    const offset = todayDate.getTimezoneOffset();
+    todayDate = new Date(todayDate.getTime() - offset * 60 * 1000);
+    return todayDate.toISOString().split("T")[0];
+  };
 
-    render(){
-        const {expContainer,handleEdit,placesWhereEditIsOpen} = this.props;
-        const expList = expContainer.map((exp,index)=>{
-            const id=uniqid();
-            const editIsOnForThisExp=placesWhereEditIsOpen.find((editExp)=> JSON.stringify(editExp[0])===JSON.stringify(exp));
-            //editExp=[the exp being edited,it's index in expContainer]
-            //editIsOnForThisExp has two possible value
-            //undefined: when the current exp from map loop is not being edited
-            //exp: when the current exp from map loop is being edited
-            if(editIsOnForThisExp){
-                return <Form 
-                    key={id} 
-                    editedExpNum={editIsOnForThisExp[1]} 
-                    input={this.props.input} 
-                    previousValues={editIsOnForThisExp[0]}
-                    handleDelete={this.props.handleDelete}/>
-            }
-            return(
-                <div key={id}>
-                <p>Company Name: {exp.companyName}</p>
-                <p>Position Title: {exp.positionTitle}</p>
-                <p>From: {exp.from}</p>
-                <p>To: {exp.to}</p>
-                <p>Details: { exp.details}</p>
-                <button id={index} className='editBtn' onClick={handleEdit}>Edit</button>
-                </div>
-            )
-        });
-        return <div className='workInfo'>{expList}</div>
+  const expList = expContainer.map((exp, index) => {
+    const id = uniqid();
+    const editIsOnForThisExp = placesWhereEditIsOpen.find(
+      (editExp) => JSON.stringify(editExp[0]) === JSON.stringify(exp)
+    );
+    // editExp=[the exp being edited,it's index in expContainer]
+    // editIsOnForThisExp has two possible value
+    // undefined: when the current exp from map loop is not being edited
+    // exp: when the current exp from map loop is being edited
+    if (editIsOnForThisExp) {
+      return (
+        <Form
+          key={id}
+          editedExpNum={editIsOnForThisExp[1]}
+          input={input}
+          previousValues={editIsOnForThisExp[0]}
+          handleDelete={handleDelete}
+          componentIsCalledFrom={componentIsCalledFrom}
+        />
+      );
     }
+    return (
+      <div key={id}>
+        {componentIsCalledFrom === "workExp" && (
+          <>
+            <p>Company Name: {exp.companyName}</p>
+            <p>Position Title: {exp.positionTitle}</p>
+          </>
+        )}
+        {componentIsCalledFrom === "eduExp" && (
+          <>
+            <p>School Name: {exp.schoolName}</p>
+            <p>Title of study: {exp.titleOfStudy}</p>
+          </>
+        )}
+        <p>From: {exp.from}</p>
+        {getTodayDate() === exp.to ? <p>To: Present</p> : <p>To: {exp.to}</p>}
+        {/* if exp.to is equal to today's date then display the date as 'present'
+            else display the date */}
+        <p>Details: {exp.details}</p>
+        <button
+          type="button"
+          id={index}
+          className="editBtn"
+          onClick={handleEdit}
+        >
+          Edit
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div
+      className={componentIsCalledFrom === "workExp" ? "workInfo" : "eduInfo"}
+    >
+      {expList}
+    </div>
+  );
 }
 
 export default ExperienceFactory;
