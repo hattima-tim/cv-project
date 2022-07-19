29e742adeba6fdeef2cbb365716f026f49bbd4ef
hattima-tim <irfanridwan20@gmail.com>
2022-07-19 05:27:36 +0000

    add useExpHandler custom hook
    added useExpHandler custom hook and made some changes to make
    this new addition successful

--- src/components/eduExperience/eduExperience.js
+++ src/components/eduExperience/eduExperience.js
@@ -1,124 +1,54 @@
-import {Component} from 'react';
-import Form from '../edu_work_form';
-import './eduExperience.css'
-import ExperienceFactory from '../experienceFactory';
+import Form from "../edu_work_form";
+import "./eduExperience.css";
+import ExperienceFactory from "../experienceFactory";
+import useExpHandler from "../hooks/useExpHandler";
 
-class eduExp extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      expContainer:[],
-      placesWhereEditIsOpen:[], //this will hold all the exp that are being edited
-      newExpIsBeingAdded:true,
-      displayStyleForAddNewBtn:''
-    }
-  }
+function eduExp({ sendDataToApp }) {
+  const expHandler = useExpHandler(sendDataToApp, "eduExp");
 
-  handleInput=(input,editedExpNumberForEditedSubmit)=>{
-    if(editedExpNumberForEditedSubmit!==undefined){
-      this.setState((prevState)=>({
-        expContainer:prevState.expContainer.map((exp,index)=>{
-          if(index===editedExpNumberForEditedSubmit){
-            //editedExpNumber is calculated based on it's index in expContainer
-            return input; //means this input is updated/edited,so replace it with old one
-          }else{
-            return exp; 
-          }
-        }),
-        placesWhereEditIsOpen:prevState.placesWhereEditIsOpen.filter((expWhereEditisOpen)=>{
-          return expWhereEditisOpen[1]!==editedExpNumberForEditedSubmit;
-        }),
-      }));
-      
-      setTimeout(() => {
-        this.props.sendDataToApp(this.state.expContainer, 'eduExperiences')
-      }, 0);
-
-    }else{
-      this.setState((prevState) => ({
-        expContainer: [...prevState.expContainer, input],
-        newExpIsBeingAdded: false,
-        displayStyleForAddNewBtn: 'block' //to ensure there is a add new btn under a new exp section
-      }));
-      
-      setTimeout(() => {
-        this.props.sendDataToApp(this.state.expContainer, 'eduExperiences')
-      }, 0);
-      
-    }
-  }
-
-  handleEdit=(event)=>{
-    const theExpWhereEditIsHappening=this.state.expContainer[event.target.id];
-    const expNumber=Number(event.target.id);
-    this.setState((prevState)=>({
-      placesWhereEditIsOpen:[...prevState.placesWhereEditIsOpen,[theExpWhereEditIsHappening,expNumber]]
-    }))
-  }
-
-  handleAddNewButtonClick=(event)=>{
-    event.target.style.display='none';
-    this.setState({
-      newExpIsBeingAdded:true,
-      displayStyleForAddNewBtn:'none', //to remove the btn when it is clicked once
-    });
-  }
-
-  handleDelete=(e)=>{
-    if(e.target.id!==''){
-      const indexOfExpWhoseFormBeingDeleted=Number(e.target.id);
-      this.setState((prevState)=>({
-        expContainer:prevState.expContainer.filter((exp,index)=>{
-          return index!==indexOfExpWhoseFormBeingDeleted;
-        }),
-        placesWhereEditIsOpen:prevState.placesWhereEditIsOpen.filter((exp)=>{
-          return exp[1]!==indexOfExpWhoseFormBeingDeleted;
-        })
-      }))
-    }else{
-      this.setState({
-        newExpIsBeingAdded:false,
-        displayStyleForAddNewBtn:'block',
-      })
-    }
-  }
-
-  render(){
-    return(
-        <div className='eduExp_container'>
-            <h2>Educational Experience</h2>
-            {(this.state.expContainer.toString()!=='' || this.state.newExpIsBeingAdded===false) &&
-            //newExpIsBeingAdded===false is added in upper line to show Add New
-            //button when the first exp form is deleted and thus newExpIsBeingAdded
-            //becomes false
-            <div className='expList'>
-              <ExperienceFactory 
-                input={this.handleInput} 
-                placesWhereEditIsOpen={this.state.placesWhereEditIsOpen} 
-                expContainer={this.state.expContainer} 
-                handleEdit={this.handleEdit}
-                handleDelete={this.handleDelete}
-                componentIsCalledFrom='eduExp'
-              />
-              <button className='addNewBtn' style={{display:`${this.state.displayStyleForAddNewBtn}`}} onClick={this.handleAddNewButtonClick}>Add New</button>
-            {/* AddNew btn is here and not in the experienceFactory to ensure
+  return (
+    <div className="eduExp_container">
+      <h2>Educational Experience</h2>
+      {(expHandler.state.expContainer.toString() !== "" ||
+        expHandler.state.newExpIsBeingAdded === false) && (
+        //newExpIsBeingAdded===false is added in upper line to show Add New
+        //button when the first exp form is deleted and thus newExpIsBeingAdded
+        //becomes false
+        <div className="expList">
+          <ExperienceFactory
+            input={expHandler.method.handleInput}
+            placesWhereEditIsOpen={expHandler.state.placesWhereEditIsOpen}
+            expContainer={expHandler.state.expContainer}
+            handleEdit={expHandler.method.handleEdit}
+            handleDelete={expHandler.method.handleDelete}
+            componentIsCalledFrom="eduExp"
+          />
+          <button
+            className="addNewBtn"
+            style={{ display: `${expHandler.state.displayStyleForAddNewBtn}` }}
+            onClick={expHandler.method.handleAddNewButtonClick}
+          >
+            Add New
+          </button>
+          {/* AddNew btn is here and not in the experienceFactory to ensure
             there is a add new btn under all the exp sections */}
-            </div>
-            }
-            {this.state.newExpIsBeingAdded===true  &&
-                  //[].toString()=''(an empty string)
-                  <Form 
-                    input={this.handleInput} 
-                    previousValues={this.state.expContainer}
-                    handleDelete={this.handleDelete}
-                    componentIsCalledFrom='eduExp'
-                    />
-                  //the previousValues prop in upper line is only there to prevent error in
-                  //the Form component which is previousValues is undefined.
-            }
         </div>
-    )
-  }
+      )}
+      {
+        expHandler.state.newExpIsBeingAdded === true && (
+          //[].toString()=''(an empty string)
+          <Form
+            input={expHandler.method.handleInput}
+            previousValues={expHandler.state.expContainer}
+            handleDelete={expHandler.method.handleDelete}
+            componentIsCalledFrom="eduExp"
+          />
+        )
+        //the previousValues prop in upper line is only there to prevent error in
+        //the Form component which is previousValues is undefined.
+      }
+    </div>
+  );
 }
 
 export default eduExp;
--- src/components/workExperience/workExperience.js
+++ src/components/workExperience/workExperience.js
@@ -1,111 +1,36 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Form from "../edu_work_form";
 import "./workExperience.css";
 import ExperienceFactory from "../experienceFactory";
+import useExpHandler from "../hooks/useExpHandler";
 
 function workExp({ sendDataToApp }) {
-  const [expContainer, setExpContainer] = useState([]);
-  const [placesWhereEditIsOpen, setPlacesWhereEditIsOpen] = useState([]);
-  // this^ will hold all the exp that are being edited
-  const [newExpIsBeingAdded, setNewExpIsBeingAdded] = useState(true);
-  const [displayStyleForAddNewBtn, setDisplayStyleForAddNewBtn] = useState("");
-
-  const handleInput = (input, editedExpNumberForEditedSubmit) => {
-    if (editedExpNumberForEditedSubmit !== undefined) {
-      setExpContainer((prevExpContainer) => {
-        return prevExpContainer.map((exp, index) => {
-          if (index === editedExpNumberForEditedSubmit) {
-            // editedExpNumber is calculated based on it's index in expContainer
-            return input;
-            // means this input is updated/edited,so replace it with old one
-          }
-          return exp;
-        });
-      });
-
-      setPlacesWhereEditIsOpen((prevPlacesWhereEditIsOpen) => {
-        return prevPlacesWhereEditIsOpen.filter((expWhereEditisOpen) => {
-          return expWhereEditisOpen[1] !== editedExpNumberForEditedSubmit;
-        });
-      });
-
-      setTimeout(() => {
-        sendDataToApp(expContainer, "workExperiences");
-      }, 0);
-    } else {
-      setExpContainer((prevExpContainer) => {
-        return [...prevExpContainer, input];
-      });
-      setNewExpIsBeingAdded(false);
-      setDisplayStyleForAddNewBtn("block"); // to ensure there is a add new btn under a new exp section
-
-      setTimeout(() => {
-        sendDataToApp(expContainer, "workExperiences");
-      }, 0);
-    }
-  };
-
-  const handleEdit = (event) => {
-    const theExpWhereEditIsHappening = expContainer[event.target.id];
-    const expNumber = Number(event.target.id);
-    setPlacesWhereEditIsOpen((prevPlacesWhereEditIsOpen) => {
-      return [
-        ...prevPlacesWhereEditIsOpen,
-        [theExpWhereEditIsHappening, expNumber],
-      ];
-    });
-  };
-
-  const handleAddNewButtonClick = (event) => {
-    const AddNewButton = event.target;
-    AddNewButton.style.display = "none";
-    setNewExpIsBeingAdded(true);
-    setDisplayStyleForAddNewBtn("none"); // to remove the btn when it is clicked once
-  };
-
-  const handleDelete = (e) => {
-    if (e.target.id !== "") {
-      const indexOfExpWhoseFormBeingDeleted = Number(e.target.id);
-      setExpContainer((prevExpContainer) => {
-        return prevExpContainer.filter((exp, index) => {
-          return index !== indexOfExpWhoseFormBeingDeleted;
-        });
-      });
-
-      setPlacesWhereEditIsOpen((prevPlacesWhereEditIsOpen) => {
-        return prevPlacesWhereEditIsOpen.filter((exp) => {
-          return exp[1] !== indexOfExpWhoseFormBeingDeleted;
-        });
-      });
-    } else {
-      setNewExpIsBeingAdded(false);
-      setDisplayStyleForAddNewBtn("block");
-    }
-  };
+  const expHandler = useExpHandler(sendDataToApp, "workExp");
 
   return (
     <div className="workExp_container">
       <h2>Work Experience</h2>
-      {(expContainer.toString() !== "" || newExpIsBeingAdded === false) && (
+      {(expHandler.state.expContainer.toString() !== "" ||
+        expHandler.state.newExpIsBeingAdded === false) && (
         // [].toString()=''(an empty string)
         // newExpIsBeingAdded===false is added in upper line to show Add New
         // button when the first exp form is deleted and thus newExpIsBeingAdded
         // becomes false
         <div className="expList">
           <ExperienceFactory
-            input={handleInput}
-            placesWhereEditIsOpen={placesWhereEditIsOpen}
-            expContainer={expContainer}
-            handleEdit={handleEdit}
-            handleAddNewButtonClick={handleAddNewButtonClick}
-            handleDelete={handleDelete}
+            input={expHandler.method.handleInput}
+            placesWhereEditIsOpen={expHandler.state.placesWhereEditIsOpen}
+            expContainer={expHandler.state.expContainer}
+            handleEdit={expHandler.method.handleEdit}
+            handleAddNewButtonClick={expHandler.method.handleAddNewButtonClick}
+            handleDelete={expHandler.method.handleDelete}
             componentIsCalledFrom="workExp"
           />
           <button
             className="addNewBtn"
             type="button"
-            style={{ display: `${displayStyleForAddNewBtn}` }}
-            onClick={handleAddNewButtonClick}
+            style={{ display: `${expHandler.state.displayStyleForAddNewBtn}` }}
+            onClick={expHandler.method.handleAddNewButtonClick}
           >
             Add New
           </button>
@@ -114,11 +39,11 @@ function workExp({ sendDataToApp }) {
         </div>
       )}
       {
-        newExpIsBeingAdded === true && (
+        expHandler.state.newExpIsBeingAdded === true && (
           <Form
-            input={handleInput}
-            previousValues={expContainer}
-            handleDelete={handleDelete}
+            input={expHandler.method.handleInput}
+            previousValues={expHandler.state.expContainer}
+            handleDelete={expHandler.method.handleDelete}
             componentIsCalledFrom="workExp"
           />
         )
